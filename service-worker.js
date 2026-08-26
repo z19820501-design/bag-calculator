666f5603388edd5997db44e9fbbf427628cabe8f
z19820501-design <z19820501@gmail.com>
2026-08-27 00:17:31 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const C="bag-calc-v9";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
