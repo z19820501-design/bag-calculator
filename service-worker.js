f1d5d1a62d571ad0f76f42d8cdc8774f371b9aac
z19820501-design <z19820501@gmail.com>
2026-09-22 00:51:38 +0800

    Add files via upload
    update v12 central inventory

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,4 @@
-const C="bag-calc-v9";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C="jiaxin-v12-central";
+self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
